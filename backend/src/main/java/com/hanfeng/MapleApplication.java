783a0a70449343efad1966c18395568ef5ce04e3
HanFeng <1217320569@qq.com>
2021-04-28 22:30:59 +0800

    init

---
+++ backend/src/main/java/com/hanfeng/MapleApplication.java
@@ -0,0 +1,16 @@
+package com.hanfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ * @author HanFeng
+ */
+@SpringBootApplication
+public class MapleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MapleApplication.class, args);
+    }
+}
